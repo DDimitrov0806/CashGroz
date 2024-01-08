60e999a2f0af7abd2cc74d75b9d6e1d6574b8207
altomov <adi.tomov@gmail.com>
2024-01-08 22:54:29 +0200

    Auth fields validation added

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,17 +2,35 @@ window.onload = function() {
     var form = document.querySelector("form");
 
     form.onsubmit = function(e) {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+        var username = document.getElementById("username");
+        var password = document.getElementById("password");
 
-        if(username.length < 5) {
-            alert("Username must be at least 5 characters long.");
-            e.preventDefault();
+        var usernameError = document.getElementById("usernameError");
+        var passwordError = document.getElementById("passwordError");
+        
+        var errors = false;
+
+        username.classList.remove('invalid');
+        void username.offsetWidth; // trigger a DOM reflow
+        password.classList.remove('invalid');
+        void password.offsetWidth; // trigger a DOM reflow
+        usernameError.textContent = '';
+        passwordError.textContent = '';
+
+        if(username.value.length < 5) {
+            usernameError.textContent = "Username must be at least 5 characters long.";
+            usernameError.classList.add("active");
+            username.classList.add('invalid');
+            errors = true;
         }
         
-        if(password.length < 8) {
-            alert("Password must be at least 8 characters long.");
-            e.preventDefault();
+        if(password.value.length < 8) {
+            passwordError.textContent = "Password must be at least 8 characters long.";
+            passwordError.classList.add("active");
+            password.classList.add('invalid');
+            errors = true;
         }
+
+        if(errors) e.preventDefault();
     }
 }
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,20 +1,78 @@
 window.onload = function() {
     var form = document.querySelector("form");
+    form.noValidate = true; // disable browser's default validation
 
     form.onsubmit = function(e) {
-        var email = document.getElementById("email").value;
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
+        var email = document.getElementById("email");
+        var username = document.getElementById("username");
+        var password = document.getElementById("password");
+        var confirmPassword = document.getElementById("confirmPassword");
 
-        if(!email || !username || !password || !confirmPassword) {
-            alert("All fields must be filled out.");
-            e.preventDefault();
-        } 
-        
-        if(password !== confirmPassword) {
-            alert("Passwords do not match.");
-            e.preventDefault();
+        var emailError = document.getElementById("emailError");
+        var usernameError = document.getElementById("usernameError");
+        var passwordError = document.getElementById("passwordError");
+        var confirmPasswordError = document.getElementById("confirmPasswordError");
+
+        var errors = false;
+
+        // reset the input fields' styles
+        email.classList.remove('invalid');
+        void email.offsetWidth; // trigger a DOM reflow
+        username.classList.remove('invalid');
+        void username.offsetWidth; // trigger a DOM reflow
+        password.classList.remove('invalid');
+        void password.offsetWidth; // trigger a DOM reflow
+        confirmPassword.classList.remove('invalid');
+        void confirmPassword.offsetWidth; // trigger a DOM reflow
+
+        emailError.textContent = '';
+        usernameError.textContent = '';
+        passwordError.textContent = '';
+        confirmPasswordError.textContent = '';
+
+        if(email.value === '') {
+            emailError.textContent = "Email is required.";
+            emailError.classList.add("active");
+            email.classList.add('invalid');
+            errors = true;
+        } else if(!/\S+@\S+\.\S+/.test(email.value)) {
+            emailError.textContent = "Not a valid email.";
+            emailError.classList.add("active");
+            email.classList.add('invalid');
+            errors = true;
         }
+
+        if(username.value === '') {
+            usernameError.textContent = "Username is required.";
+            usernameError.classList.add("active");
+            username.classList.add('invalid');
+            errors = true;
+        }
+
+        if(password.value === '') {
+            passwordError.textContent = "Password is required.";
+            passwordError.classList.add("active");
+            password.classList.add('invalid');
+            errors = true;
+        } else if(password.value.length < 8) {
+            passwordError.textContent = "Password must be at least 8 characters long.";
+            passwordError.classList.add("active");
+            password.classList.add('invalid');
+            errors = true;
+        }
+
+        if(confirmPassword.value === '') {
+            confirmPasswordError.textContent = "Confirm Password is required.";
+            confirmPasswordError.classList.add("active");
+            confirmPassword.classList.add('invalid');
+            errors = true;
+        } else if(password.value !== confirmPassword.value) {
+            confirmPasswordError.textContent = "Passwords do not match.";
+            confirmPasswordError.classList.add("active");
+            confirmPassword.classList.add('invalid');
+            errors = true;
+        }
+
+        if(errors) e.preventDefault();
     }
 }
