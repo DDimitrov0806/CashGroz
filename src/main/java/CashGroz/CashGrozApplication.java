c6fcfed8b773e46ea138439413964b85e418d2d9
DDimitrov0806 <ddimitrov0806@gmail.com>
2024-01-08 19:16:59 +0200

    Add skeleton project

---
+++ src/main/java/CashGroz/CashGrozApplication.java
@@ -0,0 +1,13 @@
+package CashGroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashGrozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashGrozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CashGroz/CashGrozApplicationTests.java
@@ -0,0 +1,13 @@
+package CashGroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashGrozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
